2f38c75887b5867d797822ec9cda6a8a2b8dcb92
Narcelio <narcelio.slima@gmail.com>
2025-06-11 20:29:57 -0300

    Adicionando arquivo de configuração .env, refatoração do connection.py

--- GCEToDDMain.py
+++ GCEToDDMain.py
@@ -1,32 +1,49 @@
-import sys
+import os
 from csv_file import CsvFile
 from csv_process import CsvProcess
 from connection import Connection
 from xml_file import XmlFile
-from send_xml_dojo import SendXmlDojo as sxd
+from dotenv import load_dotenv
 
 def main():
 
-    # API_KEY = "a30c6d080fa0a93d2527e285c250723f8a813da4"
-    # API_URL = "http://127.0.0.1:8080/api/v2/import-scan/"
-    connection = Connection("0.0.0.0", 5000)
+    load_dotenv("dojointegrationconfig.env")
 
-    file1, file_type1 = connection.receive_file_data()
-    file2, file_type2 = connection.receive_file_data()
+    server_ip = os.getenv("SERVER_IP")
+    green_port = int(os.getenv("GREENBONE_REPORT_SEND_PORT"))
+    dojo_port = int(os.getenv("DEFECTDOJO_PORT"))
+    api_import_path = os.getenv("DEFECTDOJO_API_IMPORT_PATH")
+    api_token = os.getenv("DEFECTDOJO_API_TOKEN")
+    product_name = os.getenv("DEFECTDOJO_PRODUCT_NAME")
+    enviroment = os.getenv("DEFECTDOJO_ENVIROMENT")
+    service_tag = os.getenv("DEFECTDOJO_SERVICE_TAG")
 
-    if file_type1 == 2 and file_type2 == 3:
-        xml_file = XmlFile(file1)
-        csv_file = CsvFile(file2, xml_file._xml_file_path)
-        csv_file.set_csv_data(CsvProcess.csv_convert_high_to_critical(csv_file.get_csv_string_array()))
-        csv_file.save_csv_file()
-        connection.post_engagement_data(csv_file.get_csv_buffered_reader())
-    elif file_type2 == 2 and file_type1 == 3:
-        xml_file = XmlFile(file2)
-        csv_file = CsvFile(file1, xml_file._xml_file_path)
-        csv_file.set_csv_data(CsvProcess.csv_convert_high_to_critical(csv_file.get_csv_string_array()))
-        csv_file.save_csv_file()
-        connection.post_engagement_data(csv_file.get_csv_buffered_reader())
+    connection = Connection(server_ip, green_port, dojo_port, api_import_path, api_token, product_name, enviroment, service_tag)
 
+    try:
+        file1, file_type1 = connection.receive_file_data()
+        file2, file_type2 = connection.receive_file_data()
+
+        if file_type1 == 2 and file_type2 == 3:
+            xml_file = XmlFile(file1)
+            csv_file = CsvFile(file2, xml_file._xml_file_path)
+            csv_file.set_csv_data(CsvProcess.csv_convert_high_to_critical(csv_file.get_csv_string_array()))
+            csv_file.save_csv_file()
+            connection.post_engagement_data(csv_file.get_csv_buffered_reader())
+        elif file_type2 == 2 and file_type1 == 3:
+            xml_file = XmlFile(file2)
+            csv_file = CsvFile(file1, xml_file._xml_file_path)
+            csv_file.set_csv_data(CsvProcess.csv_convert_high_to_critical(csv_file.get_csv_string_array()))
+            csv_file.save_csv_file()
+            connection.post_engagement_data(csv_file.get_csv_buffered_reader())
+    except Exception as e:
+        print(e)
+    finally:
+        try:
+            connection.close_connection()
+        except Exception as e:
+            print(e)
+    
 
     # xml_file = XmlFile(connection.receive_file_data()[0])
     # xml_file.set_xml_data(CsvProcess.xml_convert_high_to_critical(xml_file.get_xml_root()))
@@ -40,14 +57,6 @@ def main():
     # csv_file.save_csv_file()
     # connection.post_engagement_data(csv_file.get_csv_buffered_reader())
     # connection.close_connection()
-
-
-    # try:
-    #     file_path = sys.argv[1]
-    #     print(f"Nome do path:{file_path}")
-    # except (FileNotFoundError, IndexError) as e:
-    #     print(f"Nenhum arquivo foi passado por parametro.")
-    
     
     
 if __name__ == "__main__":
